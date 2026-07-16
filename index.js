d407a18810dfa0be58656ec31f981d4256149735
Corey Jackson <cjaxsn@gmail.com>
2026-07-16 18:06:49 -0400

    mutation

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ const resolvers = {
         game(parent) {
             return db.authors.find((g) => g.id === parent.game_id)
         }
+    },
+    Mutation: {
+        deleteGame(_, args) {
+            db.games = db.games.filter((g) => g.id !== args.id)
+
+            return db.games
+        }
     }
 }
 
