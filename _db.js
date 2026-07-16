fdfe26c1ab2512de67dd598df49d826d90432a2d
Corey Jackson <cjaxsn@gmail.com>
2026-07-16 17:42:23 -0400

    lorem ipsum unique values

--- _db.js
+++ _db.js
@@ -13,13 +13,13 @@ let authors = [
 ]
 
 let reviews = [
-  {id: '1', rating: 9, content: 'lorem ipsum', author_id: '1', game_id: '2'},
-  {id: '2', rating: 10, content: 'lorem ipsum', author_id: '2', game_id: '1'},
-  {id: '3', rating: 7, content: 'lorem ipsum', author_id: '3', game_id: '3'},
-  {id: '4', rating: 5, content: 'lorem ipsum', author_id: '2', game_id: '4'},
-  {id: '5', rating: 8, content: 'lorem ipsum', author_id: '2', game_id: '5'},
-  {id: '6', rating: 7, content: 'lorem ipsum', author_id: '1', game_id: '2'},
-  {id: '7', rating: 10, content: 'lorem ipsum', author_id: '3', game_id: '1'},
+  {id: '1', rating: 9, content: 'lorem ipsum1', author_id: '1', game_id: '2'},
+  {id: '2', rating: 10, content: 'lorem ipsum2', author_id: '2', game_id: '1'},
+  {id: '3', rating: 7, content: 'lorem ipsum3', author_id: '3', game_id: '3'},
+  {id: '4', rating: 5, content: 'lorem ipsum4', author_id: '2', game_id: '4'},
+  {id: '5', rating: 8, content: 'lorem ipsum5', author_id: '2', game_id: '5'},
+  {id: '6', rating: 7, content: 'lorem ipsum6', author_id: '1', game_id: '2'},
+  {id: '7', rating: 10, content: 'lorem ipsum7', author_id: '3', game_id: '1'},
 ]
 
 export default { games, authors, reviews }
